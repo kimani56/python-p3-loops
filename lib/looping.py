173bf8db0029559457e53496e944a90ee2012fd9
gift kimani <gift.kimani@students.moringaschool.com>
2023-08-22 01:21:02 +0300

    complete

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,34 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+   num = 10
+   while num >= 1:
+       print(num)
+       num -= 1
+   print("Happy New Year!")
 
-def square_integers(int_list):
-    # code goes here!
-    pass
+
+
+def square_integers(int_list): 
+    squared = []
+   
+    for int in int_list :
+        int_squared = int ** 2
+        squared.append(int_squared)
+
+    return squared
+        
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for num in range(1, 101):
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz")
+
+        elif num % 3 == 0  :
+            print("Fizz")
+        elif num % 5 == 0 :
+            print("Buzz")
+
+        else :
+            print(num)
+      
